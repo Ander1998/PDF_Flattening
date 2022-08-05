a9e300aaad548e3e2219ad9f537e9349596901a8
Ander1998 <91062352+Ander1998@users.noreply.github.com>
2022-08-05 10:05:56 +0200

    Update PDF_Flattening_V2.ps1

--- PDF_Flattening_V2.ps1
+++ PDF_Flattening_V2.ps1
@@ -1,33 +1,45 @@
 $fdfs = $HOME
 $values = Get-ChildItem -Path $fdfs -Filter *.fdf -File -Name -Recurse
-
 $cont = 0
+
 foreach ($value in $values) {
     $directorio = Split-Path $value
     $directorio = $fdfs + '\' + $directorio
     $ruta = $fdfs + '\' + $values[$cont]
     $text = Get-Content -Path $ruta
-    $entrada ='.\Plantillas_PDF\'
+    $entrada =$PSScriptRoot+'\Plantillas_PDF\'
     $hora = Get-Date -Format "yyyyMMdd_HHmmss"
     if ($text -match 'bajaINSSA.pdf') {
-
         $out = $directorio+'\Baja_'+$hora+'_'+$cont+'.pdf'
         $entrada = $entrada + 'bajaINSSa.pdf'
         pdftk $entrada fill_form $ruta output $out flatten
 
     } elseif ($text -match 'altaINSSa.pdf') {
-
-        $out = $directorio+'\Alta_'+$cont+'.pdf'
+        $out = $directorio+'\Alta_'+$hora+'_'+$cont+'.pdf'
         $entrada = $entrada + 'altaINSSa.pdf'
         pdftk $entrada fill_form $ruta output $out flatten
 
     } elseif ($text -match 'confINSSa.pdf') {
 
-        $out = $directorio+'\Conf_'+$cont+'.pdf'
+        $out = $directorio+'\Conf_'+$hora+'_'+$cont+'.pdf'
         $entrada = $entrada + 'confINSSa.pdf'
         pdftk $entrada fill_form $ruta output $out flatten
 
     }
-    Remove-Item $ruta
+    if (!([System.IO.File]::Exists($entrada))) {
+        if($null -eq $PSScriptRoot+'\Error_Log.txt') {
+            New-Item $PSScriptRoot+'\Error_Log.txt'
+        }
+        $plantilla = $(Split-Path $entrada -leaf)
+        'La plantilla ' + $plantilla + ' que se quiere usar no existe' | Out-File -FilePath .\Error_Log.txt -Append
+    }
+    if([System.IO.File]::Exists($out)) {
+        Remove-Item $ruta
+    } else {
+        if($null -eq $PSScriptRoot+'\Error_Log.txt') {
+            New-Item $PSScriptRoot+'\Error_Log.txt'
+        }
+        $Error | Out-File -FilePath .\Error_Log.txt -Append    
+    }
     $cont++
 }
